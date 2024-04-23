fe61e896c5b8fd866a9f3a544bf282492791bc46
Saleemullah Khan Saleem <142225115+saleemullahkhansaleem@users.noreply.github.com>
2024-04-23 17:47:18 +0500

    fix

--- src/componenets/ButtonGo.jsx
+++ src/componenets/ButtonGo.jsx
@@ -7,7 +7,7 @@ const ButtonGo = ({ children, href, onClick, className, icon }) => {
       onClick={onClick}
       className={`group/buttonGo bg-zinc-900 flex gap-2 items-center pl-3 pr-4 py-1 rounded hover:border-lime-500 cursor-pointer text-xs ${className}`}
     >
-      <h1 className="">{children}</h1>
+      <p className="">{children}</p>
       <span className="transition group-hover/buttonGo:translate-x-1 group-hover/buttonGo:text-lime-500 ease-in duration-300">
         {icon}
       </span>
