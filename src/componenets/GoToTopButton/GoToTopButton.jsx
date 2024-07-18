80a210619e41dbc57977be2d7f75b98654b08088
Saleemullah Khan Saleem <142225115+saleemullahkhansaleem@users.noreply.github.com>
2024-07-18 18:00:59 +0500

    fix goToTop button

--- src/componenets/GoToTopButton/GoToTopButton.jsx
+++ src/componenets/GoToTopButton/GoToTopButton.jsx
@@ -1,42 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { FaArrowUp } from "react-icons/fa";
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
 
 const GoToTopButton = () => {
   const [showButton, setShowButton] = useState(false);
 
-  // Function to scroll to the top of the page
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+  const handleScroll = useCallback(() => {
+    const threshold = 300; // Adjust this threshold as needed
+    setShowButton(window.scrollY > threshold);
+  }, []);
 
   useEffect(() => {
-    // Add scroll event listener to show/hide the button
-    const handleScroll = () => {
-      const threshold = 300; // Adjust this threshold as needed
-      if (window.scrollY > threshold) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
-      }
-    };
-
     window.addEventListener("scroll", handleScroll);
-
-    // Remove event listener on component unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [handleScroll]);
 
   return (
     <button
-    title="Go to top"
-      aria-label="Goto Top"
-      className={`fixed bottom-6 left-8 flex justify-center items-center text-sm cursor-pointer opacity-0 transition duration-300 ${
-        showButton ? "opacity-100" : ""
+      title="Go to top"
+      aria-label="Go to Top"
+      className={`fixed bottom-8 left-8 text-sm cursor-pointer transition-opacity duration-300 ${
+        showButton ? "opacity-100" : "opacity-0"
       } hover:text-lime-500`}
       onClick={scrollToTop}
     >
