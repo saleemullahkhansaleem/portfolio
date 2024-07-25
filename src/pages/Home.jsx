1e66d304234ebace10f720265a37393b3312bf6b
Saleemullah Khan Saleem <142225115+saleemullahkhansaleem@users.noreply.github.com>
2024-07-25 09:59:09 +0500

    organize tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,23 +7,23 @@ const Home = () => {
   return (
     <Container
       id="home"
-      className="md:min-h-screen flex flex-wrap justify-between"
+      className="flex flex-wrap justify-between md:min-h-screen"
     >
-      <div className="w-full sm:w-full md:w-4/12 flex items-center">
-        <div className="md:w-full w-48 md:h-auto h-48 bg-black md:bg-transparent rounded overflow-hidden m-auto">
+      <div className="flex items-center w-full sm:w-full md:w-4/12">
+        <div className="w-48 h-48 m-auto overflow-hidden bg-black rounded md:w-full md:h-auto md:bg-transparent">
           <img
             title="Mr Saleem"
             alt="Profile Image"
             id="profile-img"
             src="/profile.png"
-            className="w-full max-w-sm border-b m-auto"
+            className="w-full max-w-sm m-auto border-b border-zinc-500"
           />
         </div>
         {/* <SocialLinks data={Data.contactLinks} className="mt-4" /> */}
         {/* <SocialLinks className="mt-4" iconSize="lg" data={Data.socialLinks} /> */}
       </div>
-      <div className="w-full md:w-8/12 flex flex-col justify-center gap-10 p-8">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight">
+      <div className="flex flex-col justify-center w-full gap-10 p-8 md:w-8/12">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
           Hi From <br />
           <span className="text-lime-500">Saleemullah Khan Saleem,</span>
           <br />
